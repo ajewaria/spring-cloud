46375e47280726af76736289b1ad8444472fb2fa
Arun Jewaria <arunjewaria@gmail.com>
2025-06-15 10:21:44 +0530

    Add Intial project setup for config

---
+++ config/config-client/src/test/java/aj/spring/cloud/config/client/config_client/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package aj.spring.cloud.config.client.config_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ config/config-server/src/main/java/aj/spring/cloud/config/server/ConfigServerConfig.java
@@ -0,0 +1,8 @@
+package aj.spring.cloud.config.server;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigServerConfig {
+    
+}
